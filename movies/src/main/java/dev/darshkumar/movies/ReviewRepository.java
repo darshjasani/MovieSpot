98fc9bdc0197d64ebbd713575377490579db546a
Darshkumar Jasani <jasanidarsh3@gmail.com>
2024-05-13 16:44:48 -0700

    Reverting back

--- movies/src/main/java/dev/darshkumar/movies/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.darshkumar.movies;
-
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-import java.util.List;
-@Repository
-public interface ReviewRepository extends MongoRepository<Review, ObjectId>{
-}
